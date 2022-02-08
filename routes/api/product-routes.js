23c9ddb2748c338a8e3e97a3984e7ac307d9ce7d
sinka27 <sinka27392@gmail.com>
2022-02-07 21:57:26 -0800

    DELETE request for deleting product by its Id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,6 +106,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((product) => res.status(200).json("Product deleted"))
+  .catch((err) => res.status(400).json(err))
 });
 
 module.exports = router;
