6f4d7fa3ffa623fdd08a7fc6a0602c96a32478b0
sinka27 <sinka27392@gmail.com>
2022-02-07 19:22:10 -0800

    Defined columns in products table

--- models/Product.js
+++ models/Product.js
@@ -8,9 +8,45 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
+  
     // define columns
-  },
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        defaultValue:'',
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true,
+        validate: {
+          isDecimal: true,   
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validation:{
+          isNumeric: true,
+        },
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+          unique: false
+        },
+      },
+  
+    },
   {
     sequelize,
     timestamps: false,
