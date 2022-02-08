9b9467b5f8aad8596d964ca0cd69817498372cb4
sinka27 <sinka27392@gmail.com>
2022-02-07 21:47:03 -0800

    GET request for finding one product by its Id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,7 +18,16 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id:req.params.id
+    },
+    // included its associated Category and Tag data
+    include: [Category, { model: Tag, through: ProductTag }]
+  })
+
+  .then((product) => res.status(200).json(product))
+  .catch((err) => res.status(400).json(err))
 });
 
 // create new product
