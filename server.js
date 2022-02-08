62428d369a573d624870fbb8cf435317f63abd40
sinka27 <sinka27392@gmail.com>
2022-02-07 22:24:50 -0800

    Removed commented part app.listen

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
-
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
